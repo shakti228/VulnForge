2a322efd5e21abe13800e7db40d0294b085dc7f4
VYZENTRA <shaktimalik463@gmail.com>
2026-08-25 00:56:03 +0000

    test: add VulnForge v0.3 regression coverage

--- vulnforge/config.py
+++ vulnforge/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Config:
     app_name: str = "VulnForge"
-    version: str = "0.2.0"
+    version: str = "0.3.0"
     author: str = "VYZENTRA"
     report_dir: str = "reports"
 
